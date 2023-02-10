070818e7f43b8675459342dfd3972fbca3f8a099
jfnanezguaca <jfnanezguaca@gmail.com>
2023-02-10 13:07:50 -0500

    Inicio 10/02/2023

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
-  heroes: string [] = ['SpiderMan','Ironman','Hulk','Thor'];
+  heroes: string [] = ['SpiderMan','Ironman','Hulk','Thor','Captain America'];
+  tempHero: string = '';
+
+
+  deleteHero(): void {
+
+    console.log("Erasing...")
+
+    let length: number = (this.heroes.length - 1);
+    this.tempHero = this.heroes[length];
+
+    this.heroes.splice(length,1);
+    // this.heroes.pop(); Elimina el último elemento
+
+    console.log('Removal Complete');
+    console.log('Aquí hay: '+ this.heroes[length]);
+    console.log('El tamaño es: '+ this.heroes.length);
+  }
 
 }
